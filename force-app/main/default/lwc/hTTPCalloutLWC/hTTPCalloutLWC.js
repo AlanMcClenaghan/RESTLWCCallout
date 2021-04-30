7de974c072c4218474e7ffe4e379ab9e0b0f1687
Alan McClenaghan <alandmcclenaghan@icloud.com>
2021-04-30 15:28:17 +0100

    Added values and variables to JS file.

--- force-app/main/default/lwc/hTTPCalloutLWC/hTTPCalloutLWC.js
+++ force-app/main/default/lwc/hTTPCalloutLWC/hTTPCalloutLWC.js
@@ -1,3 +1,16 @@
 import { LightningElement } from 'lwc';
 
-export default class HTTPCalloutLWC extends LightningElement {}
+const options = [
+    {label: 'CAD', value: 'CAD'},
+    {label: 'EUR', value: 'EUR'},
+    {label: 'GBP', value: 'GBP'},
+    {label: 'INR', value: 'INR'},
+    {label: 'USD', value: 'USD'}
+]
+
+export default class HTTPCalloutLWC extends LightningElement {
+    fromCurrencyValue;
+    toCurrencyOptions;
+    fromCurrencyOptions = options;
+    toCurrencyOptions = options;
+}
