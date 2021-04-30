581e8f61ed44c5b2eb09694fc6157cb3fcc0f65f
Alan McClenaghan <alandmcclenaghan@icloud.com>
2021-04-30 19:30:50 +0100

    REST API callout from Client Side controller

--- force-app/main/default/lwc/hTTPCalloutLWC/hTTPCalloutLWC.js
+++ force-app/main/default/lwc/hTTPCalloutLWC/hTTPCalloutLWC.js
@@ -1,8 +1,6 @@
 import { LightningElement } from 'lwc';
 
 export default class HTTPCalloutLWC extends LightningElement {
-    fromCurrencyValue;
-    toCurrencyOptions;
     options = [
         {label: 'CAD', value: 'CAD'},
         {label: 'EUR', value: 'EUR'},
@@ -10,7 +8,9 @@ export default class HTTPCalloutLWC extends LightningElement {
         {label: 'INR', value: 'INR'},
         {label: 'USD', value: 'USD'}
     ]
-    exchangeRateData;
+    fromCurrencyValue;
+    toCurrencyOptions;
+    displayData;
 
     handleFromCurrencyChange(event) {
         this.fromCurrencyValue = event.target.value;
@@ -32,10 +32,49 @@ export default class HTTPCalloutLWC extends LightningElement {
         .then(response=>response.json())
         .then(data=>{
             console.log(data);
-            this.exchangeRateData = data;
-            console.log(this.exchangeRateData);
+
+            let tempData = {
+                From_Currency_Code : '',
+                From_Currency_Name : '',
+                To_Currency_Code : '',
+                To_Currency_Name : '',
+                Last_Refreshed : '',
+                Exchange_rate : ''
+            }
+
+            let exchangeData = data['Realtime Currency Exchange Rate'];
+            tempData.From_Currency_Code = exchangeData['1. From_Currency Code'];
+            tempData.From_Currency_Name = exchangeData['2. From_Currency Name'];
+            tempData.To_Currency_Code = exchangeData['3. To_Currency Code'];
+            tempData.To_Currency_Name = exchangeData['4. To_Currency Name'];
+            tempData.Exchange_rate  = exchangeData['5. Exchange Rate'];
+            tempData.Last_Refreshed = exchangeData['6. Last Refreshed'];
+            this.displayData = tempData;
+
+            console.log('exchangeData:' + JSON.stringify(exchangeData));
+            console.log('tempData:' + JSON.stringify(displayData));
+            console.log('this.displayData:' + JSON.stringify(this.displayData));
         })
         .catch(error => console.error(error))
     }
 
 }
+
+/*
+
+exchangeData:{
+:"INR",
+:"Indian Rupee",
+"3. To_Currency Code":"EUR",
+:"Euro",
+:"0.01122900",
+:"2021-04-30 17:27:07",
+"7. Time Zone":"UTC",
+"8. Bid Price":"0.01122870",
+"9. Ask Price":"0.01122950"}
+
+// console.log('Data: '+ data[1]);
+// this.exchangeRateData.From_Currency_Name = data['2. From_Currency Name']
+// console.log(this.exchangeRateData);
+
+*/
