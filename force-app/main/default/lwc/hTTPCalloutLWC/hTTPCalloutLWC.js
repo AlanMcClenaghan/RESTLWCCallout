06d105c0a5469f172d4adce28055ab57307fe048
Alan McClenaghan <alandmcclenaghan@icloud.com>
2021-04-30 16:07:07 +0100

    Added change handlers and clean up.

--- force-app/main/default/lwc/hTTPCalloutLWC/hTTPCalloutLWC.js
+++ force-app/main/default/lwc/hTTPCalloutLWC/hTTPCalloutLWC.js
@@ -1,16 +1,24 @@
 import { LightningElement } from 'lwc';
 
-const options = [
-    {label: 'CAD', value: 'CAD'},
-    {label: 'EUR', value: 'EUR'},
-    {label: 'GBP', value: 'GBP'},
-    {label: 'INR', value: 'INR'},
-    {label: 'USD', value: 'USD'}
-]
-
 export default class HTTPCalloutLWC extends LightningElement {
     fromCurrencyValue;
     toCurrencyOptions;
-    fromCurrencyOptions = options;
-    toCurrencyOptions = options;
+    options = [
+        {label: 'CAD', value: 'CAD'},
+        {label: 'EUR', value: 'EUR'},
+        {label: 'GBP', value: 'GBP'},
+        {label: 'INR', value: 'INR'},
+        {label: 'USD', value: 'USD'}
+    ]
+
+    handleFromCurrencyChange(event) {
+        this.fromCurrencyValue = event.target.value;
+        console.log('fromCurrencyValue ' + this.fromCurrencyValue);
+    }
+
+    handleToCurrencyChange(event) {
+        this.toCurrencyOptions = event.target.value;
+        console.log('toCurrencyOptions ' + this.toCurrencyOptions);
+    }
+
 }
