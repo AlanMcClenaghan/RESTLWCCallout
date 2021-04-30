80b72b31fdbeabeb0f25422bcb64e916057e095c
Alan McClenaghan <alandmcclenaghan@icloud.com>
2021-04-30 16:34:11 +0100

    handleCurrencyConversion and clean up

--- force-app/main/default/lwc/hTTPCalloutLWC/hTTPCalloutLWC.js
+++ force-app/main/default/lwc/hTTPCalloutLWC/hTTPCalloutLWC.js
@@ -10,6 +10,7 @@ export default class HTTPCalloutLWC extends LightningElement {
         {label: 'INR', value: 'INR'},
         {label: 'USD', value: 'USD'}
     ]
+    exchangeRateData;
 
     handleFromCurrencyChange(event) {
         this.fromCurrencyValue = event.target.value;
@@ -17,8 +18,24 @@ export default class HTTPCalloutLWC extends LightningElement {
     }
 
     handleToCurrencyChange(event) {
-        this.toCurrencyOptions = event.target.value;
-        console.log('toCurrencyOptions ' + this.toCurrencyOptions);
+        this.toCurrencyValue = event.target.value;
+        console.log('toCurrencyValue ' + this.toCurrencyValue);
+    }
+
+    handleCurrencyConversion() {
+        // REST API call
+        fetch('https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency='
+            + this.fromCurrencyValue 
+            + '&to_currency=' 
+            + this.toCurrencyValue 
+            + '&apikey=C2LE11OK1EV4V4NH')
+        .then(response=>response.json())
+        .then(data=>{
+            console.log(data);
+            this.exchangeRateData = data;
+            console.log(this.exchangeRateData);
+        })
+        .catch(error => console.error(error))
     }
 
 }
