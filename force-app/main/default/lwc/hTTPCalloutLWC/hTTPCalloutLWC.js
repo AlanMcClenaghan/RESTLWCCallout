61e270fa6153e65010427be989ab48780c97dd61
Alan McClenaghan <alandmcclenaghan@icloud.com>
2021-05-01 14:04:21 +0100

    Server-side HTTP solution added.

--- force-app/main/default/lwc/hTTPCalloutLWC/hTTPCalloutLWC.js
+++ force-app/main/default/lwc/hTTPCalloutLWC/hTTPCalloutLWC.js
@@ -1,4 +1,5 @@
 import { LightningElement } from 'lwc';
+import getCurrencyData from '@salesforce/apex/CurrencyConversionController.retrieveCurrencyConversionRates'
 
 export default class HTTPCalloutLWC extends LightningElement {
     options = [
@@ -23,7 +24,40 @@ export default class HTTPCalloutLWC extends LightningElement {
     }
 
     handleCurrencyConversion() {
-        // REST API call
+
+        // SERVER-SIDE HTTP API call
+
+        let endpoint = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency='
+                        + this.fromCurrencyValue 
+                        + '&to_currency=' 
+                        + this.toCurrencyValue 
+                        + '&apikey=C2LE11OK1EV4V4NH'
+
+        getCurrencyData({endPointURL : endpoint})
+        .then(data=>{
+            console.log(data);
+
+            let tempData = {
+                From_Currency_Code : '',
+                From_Currency_Name : '',
+                To_Currency_Code : '',
+                To_Currency_Name : '',
+                Last_Refreshed : '',
+                Exchange_rate : ''
+            }
+
+            let exchangeData = data['Realtime Currency Exchange Rate'];
+            tempData.From_Currency_Code = exchangeData['1. From_Currency Code'];
+            tempData.From_Currency_Name = exchangeData['2. From_Currency Name'];
+            tempData.To_Currency_Code = exchangeData['3. To_Currency Code'];
+            tempData.To_Currency_Name = exchangeData['4. To_Currency Name'];
+            tempData.Exchange_rate  = exchangeData['5. Exchange Rate'];
+            tempData.Last_Refreshed = exchangeData['6. Last Refreshed'];
+            this.displayData = tempData;
+        })
+        .catch(error => console.error(error))
+
+        /* // CLIENT-SIDE REST API call
         fetch('https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency='
             + this.fromCurrencyValue 
             + '&to_currency=' 
@@ -50,12 +84,9 @@ export default class HTTPCalloutLWC extends LightningElement {
             tempData.Exchange_rate  = exchangeData['5. Exchange Rate'];
             tempData.Last_Refreshed = exchangeData['6. Last Refreshed'];
             this.displayData = tempData;
-
-            console.log('exchangeData:' + JSON.stringify(exchangeData));
-            console.log('tempData:' + JSON.stringify(displayData));
-            console.log('this.displayData:' + JSON.stringify(this.displayData));
         })
-        .catch(error => console.error(error))
+        .catch(error => console.error(error)) */
+
     }
 
 }
